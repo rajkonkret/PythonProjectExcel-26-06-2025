4f98f81526a2f53325adb9b81e8ec4da387dbf2a
rajkonkret <rajkonkret660@gmail.com>
2025-06-26 12:03:08 +0200

    [1 2] add change

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # (.venv) PS C:\Users\cscomarch\PycharmProjects\PythonProjectExcel-26-06-2025> python .\main.py
 # Hi, PyCharm
+# (.venv) PS C:\Users\cscomarch\PycharmProjects\PythonProjectExcel-26-06-2025> python .\main.py
+# Hi, PyCharm
+# snake_case
