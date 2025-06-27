98be5033e9cc354ad67bc8ebde262aca1d26ca43
rajkonkret <rajkonkret660@gmail.com>
2025-06-27 15:07:47 +0200

    [2 43] add ex25.py

---
+++ day_2/ex25.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    "Imie": ["Anna", "Tomek", "Jan", "Anna", "Jan"],
+    "Miasto": ["Kraków", "Warszawa", "Gdańsk", "Kraków", "Gdańsk"],
+    "Wynik": [88, 95, 70, 91, 60],
+})
