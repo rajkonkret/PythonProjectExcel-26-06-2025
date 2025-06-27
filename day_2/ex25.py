10a3c81be85013e63642758b4794bd9e610386de
rajkonkret <rajkonkret660@gmail.com>
2025-06-27 15:19:51 +0200

    [2 44] add ex25.py

--- day_2/ex25.py
+++ day_2/ex25.py
@@ -5,3 +5,56 @@ df = pd.DataFrame({
     "Miasto": ["Kraków", "Warszawa", "Gdańsk", "Kraków", "Gdańsk"],
     "Wynik": [88, 95, 70, 91, 60],
 })
+
+df_sorted = df.sort_values(by="Imie")
+print(df_sorted.head)
+# <bound method NDFrame.head of     Imie    Miasto  Wynik
+# 0   Anna    Kraków     88
+# 3   Anna    Kraków     91
+# 2    Jan    Gdańsk     70
+# 4    Jan    Gdańsk     60
+# 1  Tomek  Warszawa     95>
+
+df_sorted_desc = df.sort_values(by="Imie", ascending=False)
+print(df_sorted_desc)
+# 1  Tomek  Warszawa     95>
+#     Imie    Miasto  Wynik
+# 1  Tomek  Warszawa     95
+# 2    Jan    Gdańsk     70
+# 4    Jan    Gdańsk     60
+# 0   Anna    Kraków     88
+# 3   Anna    Kraków     91
+
+df_sorted_multi = df.sort_values(by=['Miasto', "Wynik"], ascending=[True, False])
+print(df_sorted_multi.head)
+# <bound method NDFrame.head of     Imie    Miasto  Wynik
+# 2    Jan    Gdańsk     70
+# 4    Jan    Gdańsk     60
+# 3   Anna    Kraków     91
+# 0   Anna    Kraków     88
+# 1  Tomek  Warszawa     95>
+
+df_grouped_mean = df.groupby("Imie", as_index=False)["Wynik"].mean()
+print(df_grouped_mean)
+#     Imie  Wynik
+# 0   Anna   89.5
+# 1    Jan   65.0
+# 2  Tomek   95.0
+
+df_grouped_sum = df.groupby("Imie", as_index=False)["Wynik"].sum()
+print(df_grouped_sum)
+# 0   Anna    179
+# 1    Jan    130
+# 2  Tomek     95
+
+df_grouped_count = df.groupby("Imie").size().reset_index(name="Ilość wpisów")
+print(df_grouped_count)
+#     Imie  Ilość wpisów
+# 0   Anna             2
+# 1    Jan             2
+# 2  Tomek             1
+
+with pd.ExcelWriter('raport.xlsx', engine='xlsxwriter') as writer:
+    df.to_excel(writer, sheet_name="Oryginalne", index=False)
+    df_sorted.to_excel(writer, sheet_name="Posortowane", index=False)
+    df_grouped_mean.to_excel(writer, sheet_name="Średnie", index=False)
