e6cd4b3aae0ede9fb35941c46c5e151ef0d08e76
rajkonkret <rajkonkret660@gmail.com>
2025-06-27 09:33:24 +0200

    [2 1] add ex6

---
+++ day_1/ex6.py
@@ -0,0 +1,24 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('videogamesales2.xlsx')
+# ws = wb.active
+
+ws = wb['vgsales']
+
+# new_row = (1, 'The Legend of Zelda', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51, 6.5)
+# ws.append(new_row)
+#
+# wb.save('videogamesales2.xlsx')
+# wb.close()
+
+# odczyt
+values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
+print(values)
+# [1, 'The Legend of Zelda', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51, 6.5]
+
+# usunięcie jednego wiersza(ostatniego)
+
+ws.delete_rows(ws.max_row - 1, 1)
+
+wb.save('videogamesales2.xlsx')
+wb.close()
